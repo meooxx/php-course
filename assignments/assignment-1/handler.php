73c137a6da49458afd6398e68c6086f0eec7ca80
Chiul(shaoqiu) <13815865+meooxx@users.noreply.github.com>
2026-05-30 16:04:58 -0400

    Add Handler class for Dog API integration with GET request method

---
+++ assignments/assignment-1/handler.php
@@ -0,0 +1,36 @@
+<?php
+require_once "config.php";
+
+class Handler
+{
+
+	private $path = "";
+	public function __construct($endPoint)
+	{
+		$this->path = DOG_API_BASE_URL . $endPoint;
+
+	}
+	public function get($query)
+	{
+		// include the API key in the request header
+		$options = [
+			"http" => [
+				"method" => "GET",
+				"header" => [
+					"X-API-KEY: " . DOG_API_KEY,
+				]
+			]
+		];
+		$url = $this->path . "?" . $query;
+		error_log("url: " . $url);
+		// error_log("params: " . json_encode($options));
+		$rawJsonString = @file_get_contents($url, false, stream_context_create($options));
+		if ($rawJsonString === false) {
+			return [];
+		}
+		return json_decode($rawJsonString);
+	}
+
+}
+
+?>
